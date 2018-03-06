e5190e1d0e4ef0a69027932317f064450c5d1ddc
JerraH <jerra.haynes@gmail.com>
2018-03-06 11:26:56 -0500

    ok here we go

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ const app = express();
 
 
 app.get('/', (req, res) => {
+    let myStatus = res.status();
+    if (!myStatus.socket.Socket._hadError) {
+        myStatus = res.status(200);
+    }
+    console.log('someone has made a GET request for the route!', myStatus.statusCode);
     res.status(200).send('HELLO I LOVE CATS!!!')
 } )
 
 app.get('/cats', (req, res) => {
-    res.sendStatus(200)
+    console.log('someone has made a GET request for CATS!!!');
+    res.send('MORE CATS!!!')
 } )
+
+app.get('/news', (req, res) => {
+    console.log("someone has made a GET request for NEWS");
+    res.send('CNN please stop sending me breaking news alerts it has become very distracting')
+})
+
+
 
 
 
